6211ffca7432cfaa319fb9d3f7304db402396dcb
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-12-19 20:59:35 +0100

    mise en page

--- src/Form/EvenementsType.php
+++ src/Form/EvenementsType.php
@@ -6,6 +6,7 @@ use App\Entity\Evenements;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,8 @@ class EvenementsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class, array(
+                'label' => 'Nom de l\'évenement',))
             ->add('date', DateType::class, [
                 'widget' => 'single_text',
                 'invalid_message' => 'La date doit être 
@@ -24,6 +26,7 @@ class EvenementsType extends AbstractType
 
             ])
             ->add('hour', TimeType::class, array(
+                'label' => 'Heure',
                 'input'  => 'datetime',
                 'widget' => 'choice',))
         ;
